2e2037194180aad0c758c966e3f8d0e0c9c80d80
Kyrylo Melnychuk <km@uspacy.com>
2023-08-25 11:13:24 +0100

    Add CreateExternalLineRequest

---
+++ app/Http/Integrations/Uspacy/Requests/Messenger/CreateExternalLineRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Integrations\Uspacy\Requests\Messenger;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Traits\Body\HasJsonBody;
+
+class CreateExternalLineRequest extends Request  implements HasBody
+{
+    use HasJsonBody;
+    /**
+     * Define the HTTP method
+     *
+     * @var Method
+     */
+    protected Method $method = Method::POST;
+
+    public function __construct(
+        protected array $payload
+    ) {}
+
+    /**
+     * Define the endpoint for the request
+     *
+     * @return string
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/messenger/v1/external-lines';
+    }
+
+    protected function defaultBody(): array
+    {
+        return $this->payload;
+    }
+}
