6edad17928f782e67c9f5c86b75d361637c22418
Anton <medoc1948@gmail.com>
2024-06-03 11:57:26 +0300

    added request for update message

---
+++ app/Http/Integrations/Uspacy/Requests/Messenger/UpdateMessageRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Integrations\Uspacy\Requests\Messenger;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+use Saloon\Contracts\Body\HasBody;
+use Saloon\Traits\Body\HasJsonBody;
+
+class UpdateMessageRequest extends Request  implements HasBody
+{
+    use HasJsonBody;
+
+    public function __construct(
+        protected string $messageId,
+        protected array $payload
+    ) {}
+    
+    /**
+     * Define the HTTP method
+     *
+     * @var Method
+     */
+    protected Method $method = Method::PATCH;
+
+    /**
+     * Define the endpoint for the request
+     *
+     * @return string
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/messenger/v1/messages/' . $this->messageId;
+    }
+
+    protected function defaultBody(): array
+    {
+        return $this->payload;
+    }
+}
