8a9efba5e225122f5f238af1eed9b71de9be5b93
nonamich <dimaeromin97@gmail.com>
2026-01-01 22:20:40 +0200

    feat: add GetGeneralSettingsRequest class for retrieving general settings

---
+++ src/Http/Client/Requests/Settings/GetGeneralSettingsRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Uspacy\SDK\Http\Client\Requests\Settings;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class GetGeneralSettingsRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(
+        protected string $domain,
+    ) {
+    }
+
+    public function resolveEndpoint(): string
+    {
+        return "/settings/v1/settings/general?domain={$this->domain}";
+    }
+}
